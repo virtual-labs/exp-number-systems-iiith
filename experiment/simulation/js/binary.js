0dec874beafdecbcd62fc8efad8ae0bb88ceff0f
vjspranav <pranavasri@live.in>
2021-05-26 19:21:54 +0530

    Add a working binary addition

--- experiment/simulation/js/binary.js
+++ experiment/simulation/js/binary.js
@@ -1,11 +1,91 @@
 "use strict";
 
+const sleep = (s) => {
+  return new Promise((resolve) => setTimeout(resolve, s * 1000));
+};
+
 const addButton = document.getElementById("add-button");
 const mulButton = document.getElementById("mul-button");
+const ans = document.getElementById("answer-container");
 
-addButton.addEventListener("click", () => {
-  const val1 = document.getElementById("in1").value;
-  const val2 = document.getElementById("in2").value;
-  const ans = document.getElementById("answer-container");
-  ans.innerHTML = val1 + val2;
+const clear_answer_container = () => {
+  ans.style.display = "none";
+  for (let i = 1; i < 6; i += 2) {
+    if (ans.childNodes[1].childNodes[i]) {
+      ans.childNodes[1].childNodes[i].innerHTML = "";
+    }
+  }
+};
+
+const populate_questions = (val1, val2, length) => {
+  // Inserting value 1
+  let parent = document.getElementById("val-1");
+  val1 = val1.split("");
+  console.log(val1);
+  for (let i = 0; i < length; i++) {
+    let tempDiv = document.createElement("div");
+    tempDiv.id = "v1" + (length - i).toString();
+    let lastEl = val1.pop();
+    tempDiv.innerHTML = lastEl ? lastEl : 0;
+    tempDiv.classList.add("custom-cell");
+    parent.prepend(tempDiv);
+  }
+  // Inserting value 2
+  parent = document.getElementById("val-2");
+  val2 = val2.split("");
+  for (let i = 0; i < length; i++) {
+    let tempDiv = document.createElement("div");
+    tempDiv.id = "v2" + (length - i).toString();
+    let lastEl = val2.pop();
+    tempDiv.innerHTML = lastEl ? lastEl : 0;
+    tempDiv.classList.add("custom-cell");
+    parent.prepend(tempDiv);
+  }
+};
+
+const populate_answer_container = (length) => {
+  parent = document.getElementById("answer");
+  for (let i = 0; i < length; i++) {
+    let tempDiv = document.createElement("div");
+    tempDiv.classList.add("custom-cell");
+    tempDiv.id = "a" + (length - i).toString();
+    tempDiv.innerHTML = ".";
+    parent.prepend(tempDiv);
+  }
+};
+
+const answer_question = async (length) => {
+  let remainder = 0;
+  for (let i = length; i > 0; i--) {
+    let v1Div = document.getElementById("v1" + i.toString());
+    let v2Div = document.getElementById("v2" + i.toString());
+    let aDiv = document.getElementById("a" + i.toString());
+    let v1 = parseInt(v1Div.innerHTML);
+    let v2 = parseInt(v2Div.innerHTML);
+    v1Div.style.backgroundColor = "green";
+    v2Div.style.backgroundColor = "green";
+    await sleep(0.5);
+    let tSum = v1 + v2 + remainder;
+    aDiv.innerHTML = tSum > 1 ? (tSum === 3 ? 1 : 0) : tSum;
+    remainder = tSum > 1 ? 1 : 0;
+    console.log(remainder);
+    await sleep(1);
+    v1Div.style.backgroundColor = "transparent";
+    v2Div.style.backgroundColor = "transparent";
+  }
+};
+
+mulButton.addEventListener("click", async () => {
+  document.getElementById("v11").style.backgroundColor = "green";
+});
+
+addButton.addEventListener("click", async () => {
+  clear_answer_container();
+  let val1 = document.getElementById("in1").value;
+  let val2 = document.getElementById("in2").value;
+  ans.style.display = "block";
+  const length = val1.length > val2.length ? val1.length + 1 : val2.length + 1;
+  populate_questions(val1, val2, length);
+  populate_answer_container(length);
+  answer_question(length);
 });
