6d10aaf0b5a18eb2312d077864d66b8685b21b94
vjspranav <pranavasri@live.in>
2021-05-27 00:55:07 +0530

    Working unary animation

--- experiment/simulation/js/unary.js
+++ experiment/simulation/js/unary.js
@@ -1,17 +1,44 @@
 "use strict";
 
-const sleep = (ms) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+const sleep = (s) => {
+  return new Promise((resolve) => setTimeout(resolve, s * 1000));
 };
 
 const addButton = document.getElementById("add-button");
 const mulButton = document.getElementById("mul-button");
 
-addButton.addEventListener("click", () => {
+addButton.addEventListener("click", async () => {
   const val1 = document.getElementById("in1").value;
   const val2 = document.getElementById("in2").value;
   const ans = document.getElementById("answer-container");
-  ans.innerHTML = val1 + val2;
+  ans.innerHTML = "";
+  const a1 = document.createElement("div");
+  a1.id = "a1";
+  const a2 = document.createElement("div");
+  a2.id = "a2";
+  a1.innerHTML = val1;
+  a2.innerHTML = val2;
+  ans.prepend(a1);
+  ans.prepend(a2);
+  anime({
+    targets: "#a1",
+    translateX: 400,
+    easing: "easeInOutSine",
+    duration: 3000,
+  });
+  anime({
+    targets: "#a2",
+    translateX: -400,
+    easing: "easeInOutSine",
+    duration: 3000,
+  });
+  await sleep(3);
+  const a = document.createElement("div");
+  a.id = "a";
+  a.classList += "custom-center";
+  a.innerHTML = val1 + val2;
+  ans.innerHTML = "";
+  ans.append(a);
 });
 
 mulButton.addEventListener("click", async () => {
